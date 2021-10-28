12f34f05000f3b232b307e89c725b374a65ab4fe
LeonardoChaga <62520316+LeonardoChaga@users.noreply.github.com>
2021-10-28 18:20:42 -0300

    fiz aqui

--- routes/app.js
+++ routes/app.js
@@ -1,6 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
+var User = require('../models/user');
+router.get('/node-mongodb-mongoose-user', function (req,res,next){
+    res.render('node');
+})
+
+router.post('/node-mongodb-mongoose-user', function (req,res,next){
+    var emailVar = req.body.emailBody;
+    var userObject = new User({
+        firstName: 'Leo',
+        lastName: 'Chaga',
+        password: "senha",
+        email: emailVar
+    });
+    userObject.save();
+
+    res.redirect('/node-mongodb-mongoose-user');
+});
+
+
 router.get('/', function (req, res, next) {
     res.render('index');
 });
