f5f40792bc12d87c3b963f4478828978c35faee7
HenriqueMiossi <henriquemiossi@gmail.com>
2021-10-27 21:41:24 -0300

    Primeiro PDF concluído, e tá funcionando 🙏

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    templateUrl: './app.component.html'
+    templateUrl: './app.component.html',
+    styles: [
+        './app.style.css'
+    ]
 })
 export class AppComponent {
     
