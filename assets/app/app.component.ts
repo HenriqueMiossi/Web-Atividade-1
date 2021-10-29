9c8dc5ecb777db9ea311302ded0862f8c7f26002
HenriqueMiossi <henriquemiossi@gmail.com>
2021-10-29 11:14:01 -0300

    Pdf IV - A, falta os últimos dois slides

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { Person } from './auth/person.model';
 
 @Component({
     selector: 'my-app',
-    templateUrl: './app.component.html'
+    templateUrl: './app.component.html',
+    styles: []
 })
 export class AppComponent {
+
+    message = {
+        content: 'Tô ficando fera no assunto',
+        author: 'Vinicius'
+    };
+
+    pessoa1: Person = new Person(101, 'Vinicius', 25);
+    pessoa2: Person;
+    pessoa3: Person = new Person();
     
 }
