4db0f258f49d4417f8f2b80d653182388c12b468
LeonardoChaga <62520316+LeonardoChaga@users.noreply.github.com>
2021-10-31 10:57:00 -0300

    opa

--- assets/app/messages/message-input.component.ts
+++ assets/app/messages/message-input.component.ts
@@ -1,13 +1,21 @@
 import { Component } from "@angular/core";
+import { MessageService } from "./message.services";
+import { Message } from "./message.model";
+import { NgForm } from "@angular/forms";
 
 @Component({
     selector: 'app-message-input',
-    templateUrl: './message-input.component.html'
+    templateUrl: './message-input.component.html',
+    /*providers: [MessageService]*/
+
 })
 export class MessageInputComponent{
+    
+    constructor (private messageService : MessageService){}
 
-    OnSave(textoConsole: string){
-        console.log(textoConsole);
+    OnSubmit(form : NgForm){
+       
+        console.log(form);
     }
 
 }
